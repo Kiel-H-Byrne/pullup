f4df5b3abfbf7402fa90ac263406412ef83ccd65
Kiel Byrne <kbyrne@costar.com>
2021-07-09 19:52:01 -0400

    add map and theme

---
+++ next.config.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+  env: {
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY
+  },
+};
---
+++ util/constants.ts
@@ -0,0 +1,28 @@
+export enum BRAND_COLORS {
+  GREEN = "#330000"
+}
+export const BRAND_THEME = {
+  styles: {
+    global: {
+      "html, body": {
+        height: "100%",
+        margin: 0,
+        padding: 0,
+        width: "100%",
+        overflowX: "hidden",
+      },
+      "#map": {
+        height: "100%",
+      },
+      "#__next": {
+        height: "100%",
+      }
+    }
+  },
+  colors: {
+    brand: {
+      100: "#fff",
+      900: "#000"
+    }
+  }
+}
